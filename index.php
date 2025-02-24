9435c3f11e3a499d6122bbbd452fc653c39bfc5b
ConorSNES <74014503+ConorSNES@users.noreply.github.com>
2025-02-24 12:11:46 +0000

    Add banana

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
 echo "this is index for Project 1";
+
+// Load file from root.
+$file = fopen("banana.txt", "r");
+// Print file.
+echo fread($file, filesize("banana.txt"));
+fclose($file);
